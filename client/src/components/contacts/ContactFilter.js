592771d98c3919fc07206cb95464ac0b56be26df
miguelfreitas18 <miguelfreitas8000@gmail.com>
2020-08-08 04:53:07 -0300

    Deploying to production *

--- client/src/components/contacts/ContactFilter.js
+++ client/src/components/contacts/ContactFilter.js
@@ -11,6 +11,7 @@ export const ContactFilter = () => {
         if (filtered === null) {
             text.current.value = '';
         }
+        // eslint-disable-next-line
     }, []);
 
     const onChange = e => {
